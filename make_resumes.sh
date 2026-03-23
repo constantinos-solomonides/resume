ffe5cd799e6522a1ae6fc84180783e1f8b3bb974
constantinos-solomonides <constantinos.st.solomonides@gmail.com>
2026-03-23 11:59:21 +0100

    Update resume and autogenerate script
    * CV Contents
        * Add concept of `role` to differentiate description according to targeted role
        * Remove non-ASCII characters, graphics etc. to avoid confusing ATS systems
        * Remove more flashy template for same reason as above
    * CV LaTeX commands
        * Modify conditional commands to clarify
        * Give sane defaults to all commands to avoid need to check for undefined ones
        * Introduce `configuration.tex` as part of document, to make configuration cleaner
    * CV Build automation (File `make_resumes.sh`)
        * Remove option to change base file
        * Add option to pick role
        * Remove use of temporary directory and functionality around it
        * Provide functionality to ensure / enforce moving all output to the outputs directory for HTML

--- make_resumes.sh
+++ make_resumes.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ue
 BASE_DIR=$(dirname $(realpath $0))
-TEMPLATE="Constantinos_Solomonides_CV-RenderCV.tex"
+TEMPLATE="Constantinos_Solomonides_CV.tex"
+CONFIGURATION_TARGET="${BASE_DIR}/configuration.tex"
+ROLES=( softwarengineer devops generalist testengineer )
+ROLE="${ROLES[0]}"
 BASE_NAME=${BASE_NAME:-"Constantinos_Solomonides_CV"}
-TMPDIR=${TMPDIR:-"/tmp/resumes"}
 RESUMES_DIR=$(realpath ${RESUMES_DIR:-"${BASE_DIR}/../resumes-output"})
 E=${E:-}
 CDD=${CDD:-false}
@@ -16,24 +18,15 @@ NAMES[french]=francais
 KNOWN_LANGUAGES=( ${!NAMES[*]} )
 USER_LANGUAGES=()
 GENERATE=pdf
-BASE_LOCATION=""
-
-# TASKS
-# * [x] Implement usage function
-# * [x] Incorporate CDD functionality -> USE cddrequested
-# * [x] Implement Generate HTML function
-# * [x] Modify this to use exported files as well, to split file preprocessing
-# * [x] define CDD function using \cddrequested command
-# * [x] Error checking
 
 function usage(){
-    cat << END
+    cat <<-END
     -v                  Set debug mode on (`set -x` output)
     -n                  dry-run (echo, instead of running commands). Some side-effects remain
-    -t <FILENAME>       Use FILENAME as template (base CV)
     -c                  Activate CDD mode (print message that shows preference for CDD / freelance work)
     -l <LANGUAGE>       Select output language. Can be used more than once to select multiple. Known ones are \
                             ${USER_LANGUAGES[@]}
+    -r <ROLE>           Select role. Currently available are ${ROLES[@]} 
     -H                  Set HTML mode, better for text-like copy-paste
     -h                  Output this message and exit
     -<ANYTHING ELSE>    Output this message and exit with error
@@ -42,12 +35,13 @@ exit ${1}
 }
 
 
-
-while getopts "hvnt:cl:H" OPT; do
+while getopts "hvnr:cl:H" OPT; do
     case ${OPT} in
             v) set -x
         ;;  n) export E=echo
-        ;;  t) [ -e "${OPTARG}" ] && TEMPLATE="${OPTARG}"
+        ;;  r) if (export IFS=$'\n'; echo "${ROLES[*]}" | grep -q -e "${OPTARG}") ; then
+                    ROLE="${OPTARG}";
+                fi
         ;;  c) CDD=true
         ;;  l)  if (export IFS=$'\n'; echo "${KNOWN_LANGUAGES[*]}" | grep -q -e "${OPTARG}") ; then
                     USER_LANGUAGES[${#USER_LANGUAGES[*]}]="${OPTARG}";
@@ -68,20 +62,12 @@ function fix_files(){
     ${E} sed -i education.tex -e 's/\\'"'"'Education/Education/' >&2
 }
 
-function setup_location(){
-    ${E} mkdir -p "${TMPDIR}" >&2
-    location=$(${E} mktemp -d -p ${TMPDIR});
-    ${E} rsync -rvP --include='*.tex' --exclude='*' ${BASE_DIR}/ ${location}/ >&2
-    if [ -z "${E}" ]; then echo "${location}"; fi
-    if [ ! -z "${E}" ]; then echo "/tmp/safe_location"; fi
-}
-
 function prepare_file(){
-    file="${1}"
-    cdd="${2}"
-    language="${3}"
+    role="${1}"
+    language="${2}"
+    cdd="${3}"
 
-    count=0; for i in "${file}" "${cdd}" "${language}"; do
+    count=0; for i in "${role}" "${language}" "${cdd}"; do
         if [ -z "${i}" ]; then
             echo "Element at position ${count} was empty" 1>&2
             exit 1
@@ -89,25 +75,28 @@ function prepare_file(){
         count=$((count +1))
     done
 
-    ${E} sed -i "${file}" \
-        -e '/%CUSTOM_COMMAND/d' \
-        -e '/begin{document}/a\\\newcommand{\\'${language}'}{} %CUSTOM_COMMAND' >&2
-
-    if [ "${cdd,,}" == "true" ]; then
-        ${E} sed -i "${file}" \
-        -e '/begin{document}/a\\\newcommand{\\cddrequested}{} %CUSTOM_COMMAND' >&2
+    if [ -z "${CONFIGURATION_TARGET}" ]; then
+        echo "Configuration target not set. Exiting" >&2
+        exit 1
     fi
+    : > ${CONFIGURATION_TARGET}
+    ${E} cat >"${CONFIGURATION_TARGET}" <<-END
+        \\renewcommand{\\targetrole}{${role}}
+        \\renewcommand{\\cvlanguage}{${language}}
+        \\renewcommand{\\cddrequested}{${cdd}}
+END
 }
 
+
 function generate_pdf(){
-    file="${1}"
     destination="${2}"
     language="${3}"
 
-    ${E} ${PDFTEX} --output-directory="${destination}" --jobname="${BASE_NAME}_${NAMES[${language}]}" ${file} >&2
+    ${E} ${PDFTEX} --output-directory="${destination}" --jobname="${BASE_NAME}_${NAMES[${language}]}" ${TEMPLATE} >&2
 }
 
 function generate_html(){
+
     file=${1}
     destination="${2}"
     language="${3}"
@@ -116,23 +105,26 @@ function generate_html(){
     ${E} ${HTMLTEX} ${file} >&2
         {   [ ! -z "${E}" ] \
          || { [ -e "${file/tex/html}" ] \
-                &&  ${E} mv "${file/tex/html}" "${destination}/${BASE_NAME}_${NAMES[${language}]}.html" >&2 ; }; } \
+                &&  {
+                    for i in ${file/tex/*}; do
+                        [ "tex" == "${i/*./}" ] && continue;
+                        ${E} mv "${i}" "${destination}/${BASE_NAME}_${NAMES[${language}]}.${i/*./}" >&2
+                    done
+                }; }; } \
     ||  { echo "File ${file/tex/html} not created or can't be moved" 1>&2; exit 1; }
 }
 
-BASE_LOCATION=$(setup_location)
-[ -z "${BASE_LOCATION}" ] && { echo "Base location not set, exiting" 1>&2; exit 1; }
 
-pushd ${BASE_LOCATION}
+# -- MAIN functionality
 
 for language in ${USER_LANGUAGES[*]}; do
-    file=${BASE_LOCATION}/${TEMPLATE}
+    file=${BASE_DIR}/${TEMPLATE}
         {       [ ! -z "${E}"  ] \
             ||  { [ -f "${file}" ] && [ -w "${file}" ]; };
         } \
     ||  { echo "File ${file} not found or not writable, exiting " 1>&2; exit 1; }
 
-    prepare_file "${file}" "${CDD}" "${language}"
+    prepare_file "${ROLE}" "${language}" "${CDD}"
 
     case "${GENERATE}" in
             pdf)    generate_pdf "${file}" "${RESUMES_DIR}" "${language}"
